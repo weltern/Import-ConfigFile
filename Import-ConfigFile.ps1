bb4db11a1d0152ea84a1149c85e169451ef50926
Nick Welter <37944729+weltern@users.noreply.github.com>
2024-08-16 22:56:11 -0400

    Update Import-ConfigFile.ps1
    Changed SourcePath to FilePath to better fit PowerShell parameter standards

--- Import-ConfigFile.ps1
+++ Import-ConfigFile.ps1
@@ -1,9 +1,9 @@
 function Import-ConfigFile {
     <#
     .SYNOPSIS
-        Imports config file via provided SourcePath based on its file type.
+        Imports config file via provided FilePath based on its file type.
     .DESCRIPTION
-        Imports config file via provided SourcePath based on its file type to be used with dot notation and iterated through
+        Imports config file via provided FilePath based on its file type to be used with dot notation and iterated through
     .NOTES
         Author: Nick Welter
         Function Created: 03/28/2024
@@ -16,26 +16,26 @@ function Import-ConfigFile {
         .txt
     .EXAMPLE
         Import-ConfigFile.ps1 -Verbose
-        Imports config file via provided SourcePath based on its file type. 
+        Imports config file via provided FilePath based on its file type. 
     #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
-        [string]$SourcePath
+        [string]$FilePath
     )
     begin {
         #- Test-Path of Config File
         Write-Verbose -Message "Checking file path of provided config file."
-        if(!(Test-Path -Path $SourcePath -ErrorAction Stop)){
-            Write-Error -Message "File path `"$SourcePath`" does not exist or cannot be accessed."
+        if(!(Test-Path -Path $FilePath -ErrorAction Stop)){
+            Write-Error -Message "File path `"$FilePath`" does not exist or cannot be accessed."
             throw
         }else{
             try {
-                $ConfigFileSource = Get-Item -Path "$SourcePath" -Verbose -ErrorAction Stop
+                $ConfigFileSource = Get-Item -Path "$FilePath" -Verbose -ErrorAction Stop
                 Write-Verbose -Message "Config file found."
             }
             catch {
-                Write-Error -Message "Failed to get item at path '$SourcePath'. Error: $_"
+                Write-Error -Message "Failed to get item at path '$FilePath'. Error: $_"
                 throw
             }
         }
@@ -44,24 +44,24 @@ function Import-ConfigFile {
         #- import its contents
         switch ($ConfigFileSource.Extension) {
             ".json" {
-                $ConfigFile = Get-Content -Path $SourcePath | Out-String | ConvertFrom-Json 
+                $ConfigFile = Get-Content -Path $FilePath | Out-String | ConvertFrom-Json 
                 Write-Verbose -Message "$($ConfigFileSource.Name) has been imported from a JSON file"
             }
             ".xml" {
-                $XMLFile = Get-Content -Path $SourcePath; $ConfigFile = [xml]$XMLFile
+                $XMLFile = Get-Content -Path $FilePath; $ConfigFile = [xml]$XMLFile
                 Write-Verbose -Message "$($ConfigFileSource.Name) has been imported from a XML file"
             }
             ".ini" {
-                $ConfigFile = Get-Content -Path $SourcePath | Select-Object -Skip 1 | ConvertFrom-StringData
+                $ConfigFile = Get-Content -Path $FilePath | Select-Object -Skip 1 | ConvertFrom-StringData
                 Write-Verbose -Message "$($ConfigFileSource.Name) has been imported from a INI file"
             }
             Default {
                 try {
-                    $ConfigFile = Get-Content -Path $SourcePath
+                    $ConfigFile = Get-Content -Path $FilePath
                     Write-Verbose -Message "$($ConfigFileSource.Name) has been imported with default settings."
                 }
                 catch {
-                    Write-Error -Message "Failed to read the configuration file at path '$SourcePath'. Error: $_"
+                    Write-Error -Message "Failed to read the configuration file at path '$FilePath'. Error: $_"
                     throw
                 }
             }
